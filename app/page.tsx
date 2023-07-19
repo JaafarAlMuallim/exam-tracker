1f1d558a002953d7ae0853d2ebd2ee1b6985de9e
JaafarAlMuallim <Ja3far03@gmail.com>
2023-07-19 17:22:42 +0300

    updated styles and added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const data = (await res.json()) as { exams: Data[] };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center mb-5">Exam Tracker</h1>
+      {/* <h1 className="text-4xl font-bold text-center mb-5">Exam Tracker</h1> */}
       <Content data={sortByDateAndTime(data.exams)} />
     </main>
   );
