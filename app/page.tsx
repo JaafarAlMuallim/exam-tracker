54871adbd079c522698ee81aecb9b16a7f1ff6ae
JaafarAlMuallim <Ja3far03@gmail.com>
2023-07-21 23:37:46 +0300

    fixed fetch bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Content from "./components/Content";
 import { sortByDateAndTime } from "@/utils/utils";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/data");
+  const res = await fetch("https://exam-tracker.vercel.app/api/data");
   const data = (await res.json()) as { exams: Data[] };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
